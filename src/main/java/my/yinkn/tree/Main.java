8a5e3959333291af81ec71480626f8aa4519ef25
pinkyo <yinkn@outlook.com>
2018-03-18 22:37:49 +0800

    Others#, update.

--- src/main/java/my/yinkn/tree/Main.java
+++ src/main/java/my/yinkn/tree/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader fxmlLoader = new FXMLLoader();
-        Parent root = fxmlLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = fxmlLoader.load(ClassLoader.getSystemClassLoader().getResource("sample.fxml"));
         
         primaryStage.setTitle("Tree Growth");
         primaryStage.setScene(new Scene(root));
